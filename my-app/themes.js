0853166d1ca47c7739ce5c9b94f4d091cf8d6fb6
MaranathaOkeleyOdai <moodai@st.ug.edu.gh>
2024-06-27 08:22:40 +0100

    Added all files

---
+++ my-app/themes.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+    backgroundColor: "#FFFFFF",
+    textColor: "#000000",
+    textColour: "#808080",
+    tabBarBackground: "#F8F8F8",
+    tabBarActiveColor: "#007AFF",
+    tabBarInactiveColor: "#8E8E93",
+  };
+  
+  export const darkTheme = {
+    backgroundColor: "#000000",
+    textColor: "#FFFFFF",
+    textColour: "#808080",
+    tabBarBackground: "#1C1C1E",
+    tabBarActiveColor: "#0A84FF",
+    tabBarInactiveColor: "#8E8E93",
+  };
